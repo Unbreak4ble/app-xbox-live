b1a6163f7dfea4ca4b04a0b1242afcab2d9c5224
Raphael <77973453+Unbreak4ble@users.noreply.github.com>
2021-05-15 16:04:44 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const xbox = require("./Base");
 const token = require("./Base/token.js");
+const login = require("./Base/directAccount.js");
 
-module.exports.Account = xbox;
-module.exports.Token = token;
+module.exports = {
+  Account: xbox,
+  Token: token,
+  Login: login
+};
